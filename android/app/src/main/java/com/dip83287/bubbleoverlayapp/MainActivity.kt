b74f6b5f997f9e0ca56bc83563136e251cb04549
bdrdip83287 <bdrdip83287@gmail.com>
2026-03-02 04:38:25 +0600

    fix kotlin activity crash

--- android/app/src/main/java/com/dip83287/bubbleoverlayapp/MainActivity.kt
+++ android/app/src/main/java/com/dip83287/bubbleoverlayapp/MainActivity.kt
@@ -1,29 +1,26 @@
-package com.bubbleoverlayapp;
+package com.bubbleoverlayapp
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.Settings;
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import com.facebook.react.ReactActivity
 
-import com.facebook.react.ReactActivity;
+class MainActivity : ReactActivity() {
 
-public class MainActivity extends ReactActivity {
-
-  @Override
-  protected String getMainComponentName() {
-    return "main";
+  override fun getMainComponentName(): String {
+    return "main"
   }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
     if (!Settings.canDrawOverlays(this)) {
-      Intent intent = new Intent(
+      val intent = Intent(
         Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-        Uri.parse("package:" + getPackageName())
-      );
-      startActivity(intent);
+        Uri.parse("package:$packageName")
+      )
+      startActivity(intent)
     }
   }
 }
