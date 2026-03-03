48ae02634094976667c7fca0b33da02d46652fb6
bdrdip83287 <bdrdip83287@gmail.com>
2026-03-03 14:56:51 +0600

    Fix splash disappear crash - restore default onCreate

--- android/app/src/main/java/com/dip83287/bubbleoverlayapp/MainActivity.kt
+++ android/app/src/main/java/com/dip83287/bubbleoverlayapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.dip83287.bubbleoverlayapp
 
 import android.os.Bundle
-import android.widget.Toast
 import com.facebook.react.ReactActivity
 import expo.modules.ReactActivityDelegateWrapper
 
@@ -12,15 +11,6 @@ class MainActivity : ReactActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        try {
-            super.onCreate(null)
-        } catch (e: Exception) {
-            Toast.makeText(
-                this,
-                "MAIN ACTIVITY CRASH:\n${e.message}",
-                Toast.LENGTH_LONG
-            ).show()
-            e.printStackTrace()
-        }
+        super.onCreate(savedInstanceState)
     }
 }
